75e20922ce567b16babac8c2fb0db452b8b9cad7
dfukuda <daisuke.fukuda.fz@hitachi-solutions.com>
2013-08-28 17:13:26 +0900

    c

--- tmp2/src/main/java/web/Top.java
+++ tmp2/src/main/java/web/Top.java
@@ -4,6 +4,8 @@ import javax.servlet.*;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
+import com.kwanzoo.recurly.Base;
+
 import java.io.*;
 import java.sql.*;
 import java.util.*;
@@ -24,10 +26,16 @@ public class Top extends ActionBase {
 	public void action(HttpServletRequest req, HttpServletResponse res)
 			throws ServletException, IOException, SQLException {
 		
+		recurly();
 		ServletContext context = getServletContext();
 		RequestDispatcher rd = context.getRequestDispatcher("/jsp/top.jsp");
 		rd.forward(req, res);
 		return;
+		
+	}
+	
+	void recurly(){
+		Base.setAuth("daisuke.fukuda.fz@hitachi-solutions.com", "dnasis2000");
 		
 	}
 }
